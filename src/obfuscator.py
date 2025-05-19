92c590af07ea314c7bb700d182cf4a6b17f50d5f
Sarina babadi <sarinababadi900@gmail.com>
2025-05-19 16:49:17 +0330

    Renaming_identifiers and random_name are added

--- src/obfuscator.py
+++ src/obfuscator.py
@@ -1,6 +1,28 @@
 import random
 import string
 
+
 class Obfuscator:
     def __init__(self):
         self.name_map = {}
+
+    def random_name(self):
+        return ''.join(random.choices(string.ascii_lowercase, k=6))
+
+    def rename_identifiers(self, node):
+        if node.type in ('var_decl', 'param', 'id') and isinstance(node.value, tuple):
+            typename, name = node.value
+            if name not in self.name_map:
+                self.name_map[name] = self.random_name()
+            node.value = (typename, self.name_map[name])
+        elif node.type == 'id':
+            name = node.value
+            if name not in self.name_map:
+                self.name_map[name] = self.random_name()
+            node.value = self.name_map[name]
+        elif node.type == 'function_def':
+            if node.value not in self.name_map:
+                self.name_map[node.value] = self.random_name()
+            node.value = self.name_map[node.value]
+        for child in node.children:
+            self.rename_identifiers(child)
