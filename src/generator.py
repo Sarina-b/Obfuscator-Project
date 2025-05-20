b9e127874e60371dd3864c1f2d165fe4a95933d0
FATEMEHAMR <PHATEMEHAMIRABADI@GMAIL.COM>
2025-05-20 13:05:05 +0330

    test

--- src/generator.py
+++ src/generator.py
@@ -1,5 +1,5 @@
 def generate_code(node, indent=0):
-    space = '    ' * indent
+    space = '    '      *
     if node.type == 'program':
         return '\n'.join(generate_code(child, indent) for child in node.children)
     elif node.type == 'function_def':
